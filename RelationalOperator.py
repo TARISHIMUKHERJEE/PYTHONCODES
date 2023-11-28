5e1dbf7b2e1ec9df7ec20530dd01e262a762141d
TARISHIMUKHERJEE <152151146+TARISHIMUKHERJEE@users.noreply.github.com>
2023-11-28 16:40:00 +0530

    Add files via upload

---
+++ RelationalOperator.py
@@ -0,0 +1,10 @@
+a=int(input("enter number 1"))
+b=int(input("enter number 2"))
+c=int(input("enter number 3"))
+if(a>b and a>c):
+    print("a is greatest")
+if(b>a and b>c):
+    print("b is greatest")
+if(c>a and c>b):
+    print("c is greatest")
+    
