107c6b0204087c17832071fa3ec046883ccc5141
TARISHIMUKHERJEE <152151146+TARISHIMUKHERJEE@users.noreply.github.com>
2023-11-30 11:54:11 +0530

    Add files via upload

---
+++ areaofRectangle.py
@@ -0,0 +1,4 @@
+width=float(input("enter the width:    "))
+length=float(input("enter the length:  "))
+area=width*length
+print("area=",area)
---
+++ areaofSquare.py
@@ -0,0 +1,5 @@
+a=float(input("enter the length of a side:   "))
+
+area=a*a
+
+print("area=",area)
